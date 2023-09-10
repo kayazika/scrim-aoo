77d7246babd1850540b973744dc1fe73fa8a5ffa
Kaya <alexkayaalmeida@gmail.com>
2023-09-10 03:31:15 -0300

    migrations for teams and rounds

---
+++ database/migrations/2023_09_05_032713_create_rounds_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rounds', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('event_id');
+            $table->integer('team_id');
+            $table->string('team_name');
+            $table->integer('kill');
+            $table->integer('point');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rounds');
+    }
+};
